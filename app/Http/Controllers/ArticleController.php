b35ab433c6986a4d6e2582936be1ce649bbf25e4
Ali Dali <ali.dali01@stclairconnect.ca>
2020-01-29 00:56:08 -0500

    Create Article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use App\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
 
 class ArticleController extends Controller
 {
     public function index() {
         $testing = "Passing data...";
 //        $articles = DB::table('articles')->get();
-        $articles = Article::paginate(2);
+        $articles = Article::paginate(5);
         return view('articles.index', compact('articles'));
     }
 
@@ -25,6 +26,21 @@ class ArticleController extends Controller
     }
 
     public function store(Request $request) {
+        $this->validate($request, [
+            'name' => 'required',
+            'body' => 'required'
+        ]);
+
+
+        $article = new Article();
+        $article->name = $request->input('name');
+        $article->body = $request->input('body');
+        $article->author_id = 1;
+        $article->category_id = 1;
+        $article->save();
+//        dd($article);
+//        return redirect()->route('articles.show', ['article'=> $article->id]);
+        return Redirect::route('articles.show', ['article'=> $article->id]);
 
     }
 }
--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -8,12 +8,12 @@
     <form action="{{action('ArticleController@store')}}" method="post">
         <div class="form-group mt-4">
             <label for="articleName">Article name</label>
-            <input name="articleName" type="text" class="form-control" id="articleName" aria-describedby="articleName"
+            <input name="name" type="text" class="form-control" id="articleName" aria-describedby="articleName"
                    placeholder="Article name">
         </div>
         <div class="form-group">
             <label for="articleBody">Article body</label>
-            <textarea name="articleBody" class="form-control" id="articleBody" placeholder="Article Body" rows="4"></textarea>
+            <textarea name="body" class="form-control" id="articleBody" placeholder="Article Body" rows="4"></textarea>
         </div>
         <div class="float-right">
             <a class="btn btn-danger text-light" href="{{route('articles.index')}}">[x] Delete</a>
