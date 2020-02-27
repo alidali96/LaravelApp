044884827801edba85eac555d34a58acb93e99f2
Ali Dali <ali.dali01@stclairconnect.ca>
2020-02-27 10:13:10 -0500

    Foreign Keys Restraints

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,11 +9,13 @@ class DatabaseSeeder extends Seeder {
      * @return void
      */
     public function run() {
+//        Schema::disableForeignKeyConstraints();
+
         DB::table('articles')->truncate();
 //        DB::table('categories')->truncate();
         DB::table('users')->truncate();
 
-        $this->call(CategoriesTableSeeder::class);
+//        $this->call(CategoriesTableSeeder::class);
         $this->call(ArticlesTableSeeder::class);
         $this->call(UserTableSeeder::class);
     }
